4be1975ac3c6dc57debd7f4f3d8b7c0f67c44a21
Ireneus Lifard Rodriguies <138296789+Ireneusrdx@users.noreply.github.com>
2025-04-29 13:27:26 +0000

    Fix GitHub Pages deployment issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/e-plantshopping",
+    base: "/e-plantshopping/",
     plugins: [react()],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
+        sourcemap: false,
+    }
 })
