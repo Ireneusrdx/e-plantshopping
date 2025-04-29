65f5e7fc24feae54ec945f0b325e305d10a34c32
Ireneus Lifard Rodriguies <138296789+Ireneusrdx@users.noreply.github.com>
2025-04-29 13:47:08 +0000

    Updated landing page to match greenhouse image design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
@@ -18,19 +17,18 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
+           <h1>Welcome To<br />Paradise Nursery</h1>
+           <div className="divider"></div>
+           <p>Where Green Meets Serenity</p>
          
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
+           <button className="get-started-button" onClick={handleGetStartedClick}>
+             Get Started
+           </button>
          </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
-
+         <div className="aboutus_container">
+           <AboutUs/>
+         </div>
+        </div>
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList />
